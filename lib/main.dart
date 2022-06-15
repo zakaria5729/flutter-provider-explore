dcaa3de5e205b69762bd9eb2bc0753b2ab612c0a
zakaria5729 <zakariahossain143@gmail.com>
2022-06-15 17:57:33 +0600

    worked on cheap and expensive object provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'dart:collection';
+import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -129,6 +131,66 @@ class HomePage extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+@immutable
+class BaseObject {
+  final String id;
+  final String lastUpdated;
+
+  BaseObject()
+      : id = const Uuid().v4(),
+        lastUpdated = DateTime.now().toIso8601String();
+
+  @override
+  bool operator ==(covariant BaseObject other) => id == other.id;
+
+  @override
+  int get hashCode => id.hashCode;
+}
+
+@immutable
+class CheapObject extends BaseObject {}
+
+@immutable
+class ExpensiveObject extends BaseObject {}
+
+class ObjectProvider extends ChangeNotifier {
+  ObjectProvider()
+      : id = const Uuid().v4(),
+        _cheapObject = CheapObject(),
+        _expensiveObject = ExpensiveObject();
+
+  late String id;
+  late CheapObject _cheapObject;
+  late StreamSubscription _cheapObjectStreamSubs;
+
+  late ExpensiveObject _expensiveObject;
+  late StreamSubscription _expensiveObjectStreamSubs;
+
+  CheapObject get cheapObject => _cheapObject;
+  ExpensiveObject get expensiveObject => _expensiveObject;
+
+  void start() {
+    _cheapObjectStreamSubs = Stream.periodic(
+      const Duration(seconds: 1),
+    ).listen((_) {
+      _cheapObject = CheapObject();
+      notifyListeners();
+    });
+
+    _expensiveObjectStreamSubs = Stream.periodic(
+      const Duration(seconds: 10),
+    ).listen((_) {
+      _expensiveObject = ExpensiveObject();
+      notifyListeners();
+    });
+  }
+
+  void stop() {
+    _cheapObjectStreamSubs.cancel();
+    _expensiveObjectStreamSubs.cancel();
   }
 }
 
